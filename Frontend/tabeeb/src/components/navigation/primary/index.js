0f12cc3880faeb532909cc40cfd31f3c46b39f78
Ahmed Tahir Shekhani <23100197@lums.edu.pk>
2022-11-18 03:56:05 +0500

    initial routing

--- Frontend/tabeeb/src/components/navigation/primary/index.js
+++ Frontend/tabeeb/src/components/navigation/primary/index.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const PrimaryNavbar = () => {
 
     return (
@@ -9,9 +10,8 @@ const PrimaryNavbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a>About</a></li>
+        <li><a href={'/login'}>Login</a></li>
+        <li><a>Register</a></li>
       </ul>
     </div>
   </div>
--- Frontend/tabeeb/src/pages/authentication/login/index.js
+++ Frontend/tabeeb/src/pages/authentication/login/index.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import {Footer} from '../../../layouts/Footer';
-
+import Footer from '../../../layouts/Footer';
+import Header from "../../../layouts/Header";
 
 const Login = () => {
 
     return (
+
        <div>
-
-
-
+              <Header/>
+                <div>THis is Login Content</div>
+              <Footer/>
        </div>
     );
     }
