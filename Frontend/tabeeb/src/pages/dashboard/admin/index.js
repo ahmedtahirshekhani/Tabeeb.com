fbe59a581f36c763a4f768726be063c87a4194a3
Mohammad Jahanzeb Raza <jahanzebrazajafri@gmail.com>
2022-11-20 03:21:53 +0500

    designed the dynamic sidebar for navbar

--- Frontend/tabeeb/src/pages/dashboard/admin/index.js
+++ Frontend/tabeeb/src/pages/dashboard/admin/index.js
@@ -1,23 +1,26 @@
 import DashboardNavbar from "../../../components/navigation/dashboard";
 import { useEffect } from "react";
 import axios from "axios";
+import SideBar from "../../../components/sidebar";
+
 const AdminDashboard = () => {
-	useEffect(() => {
-		console.log("Admin Dashboard");
-		axios
-			.get("/api/v1/admin/home", {
-				headers: {
-					"ngrok-skip-browser-warning": "true",
-					"cache-control": "no-cache",
-				},
-			})
-			.then((res) => {
-				console.log(res.data);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	console.log("Admin Dashboard");
+	// 	axios
+	// 		.get("/api/v1/admin/home", {
+	// 			headers: {
+	// 				"ngrok-skip-browser-warning": "true",
+	// 				"cache-control": "no-cache",
+	// 			},
+	// 		})
+	// 		.then((res) => {
+	// 			console.log(res.data);
+	// 		});
+	// }, []);
 	return (
 		<>
 			<DashboardNavbar />
+			<SideBar role="doctor"/>
 		</>
 	);
 };
