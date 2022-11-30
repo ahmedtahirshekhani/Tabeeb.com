75398350693568251c3eb40815140bbaeb6da6fa
Ahmed Tahir Shekhani <23100197@lums.edu.pk>
2022-11-30 19:31:33 +0500

    library updates

--- Frontend/tabeeb/src/pages/viewpastappt/doctor.js
+++ Frontend/tabeeb/src/pages/viewpastappt/doctor.js
@@ -1,12 +1,9 @@
 import DashboardNavbar from "../../components/navigation/dashboard";
 
 import React from "react";
-import classNames from "classnames";
 import styles from "../../assets/styles/App.module.css";
 import Card from "../../components/card";
 import posts from "../../testdata";
-import fonts from  "../../index.css";
-
 
 import { useEffect } from "react";
 import axios from "axios";
@@ -26,29 +23,28 @@ const PastApptDoctor = () => {
 	}, []);
 	return (
 		<>
-			<DashboardNavbar name='Doctor Dashboard' />
-            <main className={styles.section}>
+			<DashboardNavbar name="Doctor Dashboard" />
+			<main className={styles.section}>
 				<div className="flex-1">
 					<a className="btn btn-ghost normal-case text-xl">Past Appointments</a>
 				</div>
 
 				<section className={styles.container}>
 					<div className={styles.layout}>
-					{posts.map((element, index) => (
-						<Card
-						key={index}
-						name={element.name}
-						id={element.id}
-						date={element.date}
-						time={element.time}
-						charges={element.charges}
-						prescription={element.prescription}
-
-						/>
-					))}
+						{posts.map((element, index) => (
+							<Card
+								key={index}
+								name={element.name}
+								id={element.id}
+								date={element.date}
+								time={element.time}
+								charges={element.charges}
+								prescription={element.prescription}
+							/>
+						))}
 					</div>
 				</section>
-				</main>
+			</main>
 		</>
 	);
 };
