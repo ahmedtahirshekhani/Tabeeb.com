14dc85489b01984297ca094f9f0e1ffe37ffa96b
shozabhussain <23100174@lums.edu.pk>
2022-12-10 08:53:15 +0500

    https

--- Frontend/tabeeb/src/setupProxy.js
+++ Frontend/tabeeb/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		"/api/v1",
 		createProxyMiddleware({
-			target: "https://6b14-221-120-220-12.in.ngrok.io",
+			target: "https://shoppingkart.pk",
 			changeOrigin: true,
 		})
 	);
